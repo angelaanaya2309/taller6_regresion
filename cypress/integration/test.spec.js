9dad0b089fba8e405833c01d9da6c782f60f9b2e
angelaanaya2309 <angela.anaya.castaneda@gmail.com>
2020-03-14 23:36:32 -0500

    ajuste

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -7,4 +7,12 @@ describe('Test Color', function() {
       cy.screenshot();
     });
 
+    it('limpiar paleta', function() {
+      //cy.visit('https://miso-4208-labs-vrt-colorpallete.glitch.me/palette.html');
+      cy.wait(3000);
+      cy.screenshot();
+      cy.get('#actions').contains(' Limpiar paleta').click();
+      cy.screenshot();
+    });
+
 });
